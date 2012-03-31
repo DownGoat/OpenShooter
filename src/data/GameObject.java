d54c2bfae94723f7cdd1fe234fab5cb33a2488fc
Fredrik Savland <fredrik@saevland.com>
2012-03-31 21:18:37 +0100

    Splitted game into classes
    Game is currently defunct.
    
    Signed-off-by: Fredrik Savland <fredrik@saevland.com>

---
+++ src/data/GameObject.java
@@ -0,0 +1,8 @@
+package data;
+
+public interface GameObject {
+public void moveLeft(float x, float y, float velx, float vely);
+public void moveRight(float x, float y, float velx, float vely);
+public void moveUp(float x, float y, float velx, float vely);
+public void moveDown(float x, float y, float velx, float vely);
+}
---
+++ src/data/Plane.java
@@ -0,0 +1,7 @@
+package data;
+
+public class Plane extends SuperObject {
+	public Plane() {
+
+	}
+}
