36e72b16d10410aa4415adc9341c4be0f7004f89
Fredrik Savland <fredrik@saevland.com>
2012-03-31 01:03:07 +0100

    Changed it to an Eclipse Project
    Signed-off-by: Fredrik Savland <fredrik@saevland.com>

--- src/data/Main.java
+++ src/data/Main.java
@@ -1,12 +1,12 @@
+package data;
 
-public class Foobar {
+public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
 	}
 
 }
