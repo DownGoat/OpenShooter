86dc23fa8d79e41c5a34e5b7af062b017600883c
Fredrik Savland <fredrik@saevland.com>
2012-04-01 01:28:55 +0100

    Made the plane controllable with arrowkeys

--- src/slick/OpenShooter/states/GamePlayState.java
+++ src/slick/OpenShooter/states/GamePlayState.java
@@ -10,6 +10,7 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Sound;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+
 import slick.OpenShooter.game.*;
 
 public class GamePlayState extends BasicGameState {
@@ -58,41 +59,42 @@ public class GamePlayState extends BasicGameState {
 			throws SlickException {
 		Input input = gc.getInput();
 
-		if (input.isKeyDown(Input.KEY_A)) {
-			plane.moveLeft(5, 5); // Input velocities
+		if (input.isKeyDown(Input.KEY_A) || input.isKeyDown(Input.KEY_LEFT))
+		{
+			plane.moveLeft(5, 5); //Input velocities
 		}
 
-		if (input.isKeyDown(Input.KEY_D)) {
-			plane.moveRight(5, 5); // Input velocities
+		if (input.isKeyDown(Input.KEY_D) || input.isKeyDown(Input.KEY_RIGHT)){
+		plane.moveRight(5, 5); //Input velocities
 		}
 
-		if (input.isKeyDown(Input.KEY_W)) {
-			plane.moveUp(5, 5); // Input velocities
+		if (input.isKeyDown(Input.KEY_W) || input.isKeyDown(Input.KEY_UP)) {
+		plane.moveUp(5,5); //Input velocities
 		}
-		if (input.isKeyDown(Input.KEY_S)) {
-			plane.moveDown(5, 5); // Input velocities
+		if (input.isKeyDown(Input.KEY_S) || input.isKeyDown(Input.KEY_DOWN)) {
+		plane.moveDown(5,5); //Input velocities
 		}
 		if (input.isKeyDown(Input.KEY_2)) {
 			scale += (scale >= 5.0f) ? 0 : 0.1f;
-			// plane.setCenterOfRotation(plane.getWidth() / 2.0f * scale,
-			// plane.getHeight() / 2.0f * scale);
+		//	plane.setCenterOfRotation(plane.getWidth() / 2.0f * scale,
+	//				plane.getHeight() / 2.0f * scale);
 		}
 		if (input.isKeyDown(Input.KEY_1)) {
 			scale -= (scale <= 1.0f) ? 0 : 0.1f;
-			// plane.setCenterOfRotation(plane.getWidth() / 2.0f * scale,
-			// plane.getHeight() / 2.0f * scale);
+	//		plane.setCenterOfRotation(plane.getWidth() / 2.0f * scale,
+	//				plane.getHeight() / 2.0f * scale);
 		}
 		if (input.isKeyDown(Input.KEY_SPACE)) {
 			// TODO make it shoot bullets
 			timer++;
-			if (timer > 10) {
+			if(timer > 10){
 				timer = 0;
-			} else if (timer == 1) {
-				bullet1 = new Bullet(plane.getX(), plane.getY() + 35);
-				entities.add(bullet1);
-				bullet2 = new Bullet(plane.getX() + 135, plane.getY() + 35);
-				entities.add(bullet2);
-				shot.play();
+			}else if(timer == 1){
+			bullet1 = new Bullet(plane.getX(), plane.getY()+35);
+			entities.add(bullet1);
+			bullet2 = new Bullet(plane.getX()+135, plane.getY()+35);
+			entities.add(bullet2);
+			shot.play();
 			}
 		}
 
