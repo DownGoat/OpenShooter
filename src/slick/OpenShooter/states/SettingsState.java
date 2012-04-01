662752703c45303e2654d3f32574cd8bb64bb2ee
Fredrik Savland <fredrik@saevland.com>
2012-04-02 00:51:29 +0100

    Pushed SettingsState

---
+++ src/slick/OpenShooter/states/SettingsState.java
@@ -0,0 +1,54 @@
+package slick.OpenShooter.states;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+import slick.OpenShooter.game.*;
+
+
+public class SettingsState extends BasicGameState {
+
+	/**
+	 * Menu background
+	 */
+	private Image background;
+
+
+private int stateID = -1;
+
+public SettingsState( int stateID ) 
+{
+   this.stateID = stateID;
+}
+
+@Override
+public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+	gc.setVSync(true);
+	
+	background = new Image("data/MenuBackGround.png");
+	
+}
+
+@Override
+public void render(GameContainer gc, StateBasedGame sbg, Graphics arg2)
+		throws SlickException {
+	background.draw(0, 0);
+	
+}
+
+@Override
+public void update(GameContainer arg0, StateBasedGame arg1, int arg2)
+		throws SlickException {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public int getID() {
+	return stateID;
+}
+
+}
